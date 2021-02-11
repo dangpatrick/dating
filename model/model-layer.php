59ee97f6e9b0ca74591e4d52348380ac9b2cae58
Patrick Dang <pdang2@mail.greenriver.edu>
2021-02-11 01:42:52 -0500

    added model directory and data layer with 3 functions

---
+++ model/model-layer.php
@@ -0,0 +1,24 @@
+<?php
+function getIndoor()
+{
+    $indoor = array("tv", "movies", "cooking", "board games",
+        "puzzles", "reading", "playing cards", "video games");
+    return $indoor;
+}
+
+function getOutdoor()
+{
+    $outdoor = array("hiking", "biking", "swimming", "collecting",
+        "walking", "climbing");
+    return $outdoor;
+}
+
+function getStates()
+{
+    $states = array("AL", "AK", "AZ", "AR",
+        "CA", "CO"
+    , "CT", "DE", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT",
+        "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT"
+    , "VA", "WA", "WV", "WI", "WY");
+    return $states;
+}
