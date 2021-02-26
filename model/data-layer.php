72b0eabe732404a23c6d7ddfaa5918da26e230c9
Patrick Dang <pdang2@mail.greenriver.edu>
2021-02-25 21:44:58 -0500

    added non state to validate states

--- model/data-layer.php
+++ model/data-layer.php
@@ -15,7 +15,7 @@ function getOutdoor()
 
 function getStates()
 {
-    $states = array("AL", "AK", "AZ", "AR",
+    $states = array("--","AL", "AK", "AZ", "AR",
         "CA", "CO"
     , "CT", "DE", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT",
         "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT"
